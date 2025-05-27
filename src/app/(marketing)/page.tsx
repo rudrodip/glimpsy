97eeb020912d8532c8b2f6f9243521378aac80eb
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-27 22:30:22 +0600

    refac

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,8 +2,8 @@ import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
-   <div className="space-y-10">
-    <HeroSection/>
+   <div>
+      <HeroSection/>
    </div>
   );
 }
