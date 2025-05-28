e5940a8637100e96bf15c1016e2a1bca1c6ae59b
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-28 13:35:18 +0600

    updated

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,16 @@
+import Examples from "@/components/examples";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
+import GradientBackground from "@/components/gradient-background";
 import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
-   <div>
-      <HeroSection/>
-   </div>
+    <div className="">
+      <GradientBackground className="opacity-70" />
+      <HeroSection />
+      <Examples className="mt-16 md:mt-32" />
+      <Features className="mt-16 md:mt-32" />
+    </div>
   );
 }
